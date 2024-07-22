325ff106ad576bd6b2cd17585b4f50e1ee4601e9
anthony2303 <161376491+anthony2303@users.noreply.github.com>
2024-07-22 01:30:22 -0600

    Actualizar _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -6,7 +6,7 @@ export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) 
   const chat = global.db.data.chats[m.chat];
   const bot = global.db.data.settings[this.user.jid] || {};
   if (bot.antiPrivate && !isOwner && !isROwner) {
-    await m.reply(`*[❗] Hola @${m.sender.split`@`[0]}, Esta prohibido hablar al privado del bot pero si deseas comprarlo habla al siguiente numero \nhttps://wa.me/+5215615060799
+    await m.reply(`*[❗] Hola @${m.sender.split`@`[0]}, Esta prohibido hablar al privado del bot pero si deseas comprarlo habla con mi creador \nhttps://wa.me/+584123989549
     *`, false, {mentions: [m.sender]});
     await this.updateBlockStatus(m.chat, 'block');
   }
