bd68c75b8a1b023c4b09a2620adbf2fb8b817b4d
anthony2303 <161376491+anthony2303@users.noreply.github.com>
2024-07-23 23:07:56 -0600

    Actualizar gc-hidetag.js

--- plugins/gc-hidetag.js
+++ plugins/gc-hidetag.js
@@ -36,7 +36,7 @@ const handler = async (m, {conn, text, participants, isOwner, isAdmin}) => {
     }
   }
 };
-handler.command = /^(hidetag|notificar|notify|viso)$/i;
+handler.command = /^(hidetag|notificar|n|noti|noty|notify|aviso)$/i;
 handler.group = true;
 handler.admin = true;
 export default handler;
