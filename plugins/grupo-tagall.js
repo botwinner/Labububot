80e8eb00b5b17247cb7167070b287067e83af441
anthony2303 <161376491+anthony2303@users.noreply.github.com>
2024-11-18 12:37:15 -0600

    Actualizar grupo-tagall.js

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -9,7 +9,7 @@ let teks = `╭━〔 *🥷Ay activate  te hablan 🥷* 〕━⬣\n${oi}\n\n`
 for (let mem of participants) {
 teks += `┃🥷⊹ @${mem.id.split('@')[0]}\n`}
 teks += `┃
-╰━━━━━[ *𓃠* 𝙎𝙞𝙨𝙠𝙚𝙙-𝘽𝙤𝙩 ]━━━━━⬣`
+╰━━━━━[ *𓃠* 𝙂𝙊𝙆𝙐-𝘽𝙤𝙩 ]━━━━━⬣`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )  
 }
 handler.command = /^(tagall|niñas|invocar|invocacion|todos|invocación)$/i
