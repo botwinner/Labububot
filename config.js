f8608a0e41f1731f4e9b3121ef5248cccdb351e1
anthony2303 <161376491+anthony2303@users.noreply.github.com>
2024-11-18 12:22:36 -0600

    Actualizar config.js

--- config.js
+++ config.js
@@ -12,12 +12,12 @@ import { en, es, id, ar, pt } from "./lib/idiomas/total-idiomas.js"
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 // [ES] > Agrega el número que será Bot y los que serán propietarios.
 global.owner = [
-["584123989549", 'sisket', true], //sisket
-["584123989549", 'sisked', true], //sisked
-["584123989549"]]
+["5218336105471", 'Tilin', true], //el tilin
+["5218336105471", 'gay', true], //es gay
+["5218336105471"]]
 
-global.mods = ["584123989549"]
-global.prems = ["584123989549"]
+global.mods = ["5218336105471"]
+global.prems = ["5218336105471"]
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
@@ -112,9 +112,9 @@ global.moment = moment
 // [ES] > Agregate a ti, colaboradores o ayudates, aparecerá en el comando de lista de contactos.
 // [EN] > Adding yourself, collaborators or helpers will appear in the contact list command.
 global.official = [ // Agregate si eres Owner
-["584123989549", 'sisket', 1], 
-["584123989549", 'sisked', 1],  
-["584123989549", 'siskedd', 1]]
+["5218336105471", 'tilin', 1], 
+["5218336105471", 'tilinn', 1],  
+["5218336105471", 'Tilins', 1]]
 
 global.mail = '' // Add email
 global.desc = '' // Add short description (20 caractres max)
@@ -122,8 +122,8 @@ global.desc2 = '' // Add long description (90 caractres max) (Este parámetro se
 global.country = '' // Add country, example: 🇪🇨
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 
-global.packname = "𝙎𝙞𝙨𝙠𝙚𝙙-𝘽𝙤𝙩"
-global.author = "𝙎𝙞𝙨𝙠𝙚𝙙-𝘽𝙤𝙩"
+global.packname = "𝙏𝙞𝙡𝙞𝙣-𝘽𝙤𝙩"
+global.author = "𝙏𝙞𝙡𝙞𝙣-𝘽𝙤𝙩"
 
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 // [ES] > INFORMACIÓN DE VERSIONES DEL BOT, POR FAVOR 
@@ -131,7 +131,7 @@ global.author = "𝙎𝙞𝙨𝙠𝙚𝙙-𝘽𝙤𝙩"
 // [EN] > BOT VERSION INFORMATION, PLEASE KEEP THIS UNCHANGED, WE STRIVE DAILY TO PROVIDE YOU WITH A BOT FOR THE COMMUNITY, BE GRATEFUL
 global.vs = "1.7.0"
 global.vsJB = "2.5 (Beta)"
-global.gt = "𝙎𝙞𝙨𝙠𝙚𝙙-𝘽𝙤𝙩"
+global.gt = "𝙏𝙞𝙡𝙞𝙣-𝘽𝙤𝙩"
 
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 
